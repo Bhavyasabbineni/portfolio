d722b87a5d644873a2122d26eb359ca3f8dd0997
sabbinen <sabbinenibhavya489@gmail.com>
2020-09-09 13:32:28 -0500

    Updates

---
+++ precache-manifest.3379726d0bef8499f0992fa53fe668ee.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2b4aba9f07e7ee5ac0056aa4bb806a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8963b9b9cb022e27e7e4",
+    "url": "/portfolio/static/css/main.5af26ffb.chunk.css"
+  },
+  {
+    "revision": "bd24385760eb94a40d78",
+    "url": "/portfolio/static/js/2.98691779.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/portfolio/static/js/2.98691779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8963b9b9cb022e27e7e4",
+    "url": "/portfolio/static/js/main.cf0f738b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "dc6f9adb3b57d190045353f84d5e2e62",
+    "url": "/portfolio/static/media/ATT.dc6f9adb.PNG"
+  },
+  {
+    "revision": "36ef41fbef95127578afd5af07b4237c",
+    "url": "/portfolio/static/media/avatar.36ef41fb.png"
+  },
+  {
+    "revision": "2b18a2b8af6dfb80f3a84bc622fa2258",
+    "url": "/portfolio/static/media/codememory.2b18a2b8.png"
+  },
+  {
+    "revision": "575c519b4ff2c32885d351ce0e130967",
+    "url": "/portfolio/static/media/davyschess.575c519b.png"
+  },
+  {
+    "revision": "349e4a7d7d3284a78eb5bc66475855a2",
+    "url": "/portfolio/static/media/easyq.349e4a7d.png"
+  },
+  {
+    "revision": "61e3a3939c2f93f30ac21419625c9a4f",
+    "url": "/portfolio/static/media/landing.61e3a393.jpg"
+  },
+  {
+    "revision": "84a82a2471da0da16a64f48dc4e30ac4",
+    "url": "/portfolio/static/media/meraj.84a82a24.png"
+  },
+  {
+    "revision": "4e27379201dbaa41e67079a2a66d1af0",
+    "url": "/portfolio/static/media/r6pro.4e273792.png"
+  },
+  {
+    "revision": "9760dbb74eef030024d9d99028bba47c",
+    "url": "/portfolio/static/media/roomy-mobile.9760dbb7.png"
+  },
+  {
+    "revision": "501c2695820670f8949b687b384aa0c7",
+    "url": "/portfolio/static/media/roomy.501c2695.png"
+  },
+  {
+    "revision": "9ea202df9b194a008be6b40975697c5d",
+    "url": "/portfolio/static/media/sneakerlog.9ea202df.png"
+  },
+  {
+    "revision": "206bc8d0117332d7c97ec9ef6ac7508a",
+    "url": "/portfolio/static/media/waffle-world.206bc8d0.png"
+  },
+  {
+    "revision": "b0083e99702b07e851f17d84cee654a4",
+    "url": "/portfolio/static/media/winter-sale.b0083e99.png"
+  },
+  {
+    "revision": "a6cdfdced2e7813d7c8c76de571f9d1c",
+    "url": "/portfolio/static/media/ximages.a6cdfdce.png"
+  }
+]);
